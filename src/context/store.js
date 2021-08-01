65778b09eb514677e4159b4aa2ae5fb9aa9108fb
ihsansunman@yandex.com <iS0325698741>
2021-08-01 23:31:55 +0300

    new screenshot

--- src/context/store.js
+++ src/context/store.js
@@ -4,22 +4,57 @@ const initialState = {
             id: 1,
             name: 'Defacto',
             link: 'www.defacto.com.tr',
-            vote: 10,
+            vote: 25,
             updatedDate: new Date('2021-08-01 13:03')
         },
         {
             id: 2,
             name: 'Google',
-            link: 'www.google.com.tr',
-            vote: 2,
+            link: 'www.google.com',
+            vote: 23,
             updatedDate: new Date('2021-08-01 13:09')
         },
         {
             id: 3,
             name: 'Facebook',
-            link: 'www.facebook.com.tr',
-            vote: 5,
+            link: 'www.facebook.com',
+            vote: 19,
             updatedDate: new Date('2021-08-01 13:10')
+        },
+        {
+            id: 4,
+            name: 'Yandex',
+            link: 'www.yandex.com',
+            vote: 5,
+            updatedDate: new Date('2021-08-01 13:50')
+        },
+        {
+            id: 5,
+            name: 'Youtube',
+            link: 'www.youtube.com',
+            vote: 16,
+            updatedDate: new Date('2021-08-01 14:35')
+        },
+        {
+            id: 6,
+            name: 'Amazon',
+            link: 'www.amazon.com.tr',
+            vote: 9,
+            updatedDate: new Date('2021-08-01 16:45')
+        },
+        {
+            id: 7,
+            name: 'Gittigidiyor',
+            link: 'www.gittigidiyor.com.tr',
+            vote: 0,
+            updatedDate: new Date('2021-08-01 22:10')
+        },
+        {
+            id: 8,
+            name: 'Hürriyet',
+            link: 'www.hurriyet.com.tr',
+            vote: 20,
+            updatedDate: new Date('2021-08-01 23:25')
         }
     ]
 };
